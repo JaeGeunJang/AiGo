4ff24d32a88ce6b19ba2ee6f56cb5c91b9a2a4e2
JG <jaekeun950@gmail.com>
2024-04-25 10:13:35 +0900

    RNN 모델 구성 완료

--- RNN.py
+++ RNN.py
@@ -6,7 +6,7 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
@@ -25,37 +25,57 @@ class ResidualBlock(nn.Module):
         return out
     
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks=20, num_classes=15):
         super(ResNet, self).__init__()
-        self.in_channels = 64
+        self.in_channels = 256
+        self.value_num = 256
+        self.policy_num = 2
+        self.classes = num_classes**2
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        
-        self.linear = nn.Linear(512, num_classes)
+        #initial block
+        self.conv1 = nn.Conv2d(1, self.in_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn.ReLU()
 
-    def _make_layer(self, block, out_channels, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels
-        return nn.Sequential(*layers)
+        #RNN Blocks
+        self.layers = []
+        for _ in range(num_blocks):
+            self.layers.append(block(self.in_channels, self.in_channels))
+        self.layers = nn.Sequential(*self.layers)
+
+        # Policy Block
+        self.policy_conv = nn.Conv2d(self.in_channels, self.policy_num, kernel_size=1, stride=1, padding=1, bias = False)
+        self.policy_bn = nn.BatchNorm2d(self.policy_num)
+        self.policy_relu = nn.ReLU()
+        self.policy_linear = nn.Linear((self.policy_num+num_classes)**2*self.policy_num, self.classes)  # 수정된 부분
+
+        # Value Block
+        self.value_conv = nn.Conv2d(self.in_channels, 1, kernel_size=1, stride=1, padding = 1, bias=False)
+        self.value_bn1 = nn.BatchNorm2d(1)
+        self.value_linear = nn.Linear((self.policy_num+num_classes)**2, self.value_num)  # 수정된 부분
+        self.value_relu = nn.ReLU()
+        self.value_output = nn.Tanh()
+
 
     def forward(self, x):
+        # Initial block
         out = self.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-    
 
+        # Residual blocks
+        out = self.layers(out)
+
+        # Policy head
+        policy = self.policy_relu(self.policy_bn(self.policy_conv(out)))
+        policy = policy.view(policy.size(0), -1)  # 평탄화
+        policy = self.policy_linear(policy)  # 최종 정책 출력
+
+        # Value head
+        value = self.value_relu(self.value_bn1(self.value_conv(out)))
+        value = value.view(value.size(0), -1)  # 평탄화
+        value = self.value_linear(value)
+        value = self.value_relu(value)
+        value = self.value_output(value)  # 최종 값 출력
+
+        return policy, value
+
+    
