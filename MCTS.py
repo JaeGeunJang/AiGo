85e95f0c4337e032954778451274a4b2624e3043
JG <jaekeun950@gmail.com>
2024-04-26 22:25:48 +0900

    MCTS 구성 방법 구상

--- MCTS.py
+++ MCTS.py
@@ -1,6 +1,6 @@
 import random, math
-import Rule
 
+# 기본적인 MCTS 구조. 변형 필요
 class MCTSNode:
     def __init__(self, state, player=1, parent=None, move=None):
         self.state = state
@@ -23,3 +23,12 @@ class MCTSNode:
 
     def UCB(self, mean_value, percentage, parents, child, explore = 2**0.5):
         return mean_value + explore * percentage * (parents**0.5) / (1+child)
+
+# 각 트리들은 수가 선택 된 이후 다음 스텝에도 사용되기 때문에 남아 있어야함.
+# 머리 아프네... 
+# Parents Node에 Dictionary 형식 저장 후 선택되면 나머지 삭제
+# N(s, a), Q(s, a), 
+class Node:
+    def __init__(self):
+        self.parents = None
+        self.child = None
