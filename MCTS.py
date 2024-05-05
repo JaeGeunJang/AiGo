685b15e4d685575ac9e36a8ed4a7840acabe037d
JG <jaekeun950@gmail.com>
2024-05-06 01:27:17 +0900

    주석 추가 및 주석 이동

--- MCTS.py
+++ MCTS.py
@@ -2,38 +2,9 @@ import random, math
 import numpy as np
 import torch
 
-'''
-# 기본적인 MCTS 구조. 변형 필요
-class MCTSNode:
-    def __init__(self, state, player=1, parent=None, move=None):
-        self.state = state
-        self.parent = parent
-        self.move = move
-        self.children = []
-        self.wins = 0
-        self.visits = 0
-        self.untried_move = state.ai_valid_move(player)
+# 우선 코드 완성 
+# 이제 기보 또는 state를 가지고 15X15X17의 Convolusion 생성 후 신경망에 넣어주는 작업 실시
 
-    def add_child(self, move, state):
-        child = MCTSNode(state=state, parent=self, move=move)
-        self.untried_moves.remove(move)
-        self.children.append(child)
-        return child
-    
-    def update(self, result):
-        self.visits += 1
-        self.wins += result
-
-    def UCB(self, mean_value, percentage, parents, child, explore = 2**0.5):
-        return mean_value + explore * percentage * (parents**0.5) / (1+child)
-
-
-각 트리들은 수가 선택 된 이후 다음 스텝에도 사용되기 때문에 남아 있어야함.
-머리 아프네... 
-Parents Node에 Dictionary 형식 저장 후 선택되면 나머지 삭제
-N(s, a), Q(s, a), Temperator, Prior 필요
-MCTS 코드 내에서 직접 RNN을 시행하지는 않도록 작성
-'''
 # Player는 1(흑)과 2(백)으로 설정되어 있음.
 class Node:
     def __init__(self, prior):
--- MCTS_Reference.py
+++ MCTS_Reference.py
@@ -1,5 +1,36 @@
 import random, math
+'''
+# 기본적인 MCTS 구조. 변형 필요
+class MCTSNode:
+    def __init__(self, state, player=1, parent=None, move=None):
+        self.state = state
+        self.parent = parent
+        self.move = move
+        self.children = []
+        self.wins = 0
+        self.visits = 0
+        self.untried_move = state.ai_valid_move(player)
 
+    def add_child(self, move, state):
+        child = MCTSNode(state=state, parent=self, move=move)
+        self.untried_moves.remove(move)
+        self.children.append(child)
+        return child
+    
+    def update(self, result):
+        self.visits += 1
+        self.wins += result
+
+    def UCB(self, mean_value, percentage, parents, child, explore = 2**0.5):
+        return mean_value + explore * percentage * (parents**0.5) / (1+child)
+
+
+각 트리들은 수가 선택 된 이후 다음 스텝에도 사용되기 때문에 남아 있어야함.
+머리 아프네... 
+Parents Node에 Dictionary 형식 저장 후 선택되면 나머지 삭제
+N(s, a), Q(s, a), Temperator, Prior 필요
+MCTS 코드 내에서 직접 RNN을 시행하지는 않도록 작성
+'''
 # 기본적인 MCTS 구조. 변형 필요
 class MCTSNode:
     def __init__(self, state, player=1, parent=None, move=None):
